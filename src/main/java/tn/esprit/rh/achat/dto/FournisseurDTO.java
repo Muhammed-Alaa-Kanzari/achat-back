35b55cd8476beb5120fcab9bd7d74aadb145669d
Mohammed Alaa <medalaakanzari@duck.com>
2022-11-08 17:17:05 +0100

    Add Missing Imports

--- src/main/java/tn/esprit/rh/achat/dto/FournisseurDTO.java
+++ src/main/java/tn/esprit/rh/achat/dto/FournisseurDTO.java
@@ -3,6 +3,7 @@ package tn.esprit.rh.achat.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import tn.esprit.rh.achat.entities
 
 import java.io.Serializable;
 import java.util.Set;
