60729fb72e00637e90b90d071456ca30e82455f8
Mohammed Alaa <medalaakanzari@duck.com>
2022-11-09 08:55:30 +0100

    Remove Cross Decorator

--- src/main/java/tn/esprit/rh/achat/controllers/FounisseurController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FounisseurController.java
@@ -22,7 +22,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 @Api(tags = "Gestion des fournisseurss")
 @RequestMapping("/fournisseur")
-@CrossOrigin("*")
+// @CrossOrigin("*")
 public class FounisseurController {
 	
 	
